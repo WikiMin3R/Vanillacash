e27b8595de603411dd0ab15148d657dcc7d6c6ce
WikiMin3R <60493982+WikiMin3R@users.noreply.github.com>
2023-11-05 13:37:40 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -101,7 +101,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-	nLastPOWBlock = 950000;
+	nLastPOWBlock = 920000;
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
